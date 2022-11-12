026d0933e0381e030fd9a9454444dc4bed7e5792
Fardin K <fardin.koochaki@gmail.com>
2022-11-12 19:51:09 +0330

    WIP: simjs

---
+++ cmd/simjs/main.js
@@ -0,0 +1,173 @@
+const SimSize = 256;
+const SimPopulation = 128;
+const SimEpochs = 20;
+
+const
+    GeneMaskColor = 0x0000000000000FFF,
+    GeneMaskSight = 0x000000000000F000,
+    GeneMaskSpeed = 0x00000000000F0000,
+    GeneMaskBrain = 0x0000FFFF00000000;
+const
+    ActionMove = 'move',
+    ActionEat  = 'eat',
+    ActionMate = 'mate';
+
+function rrand(min, max) {
+    return ~~(Math.random() * max + min);
+}
+
+function NewRandomAgent() {
+    return {
+        X: rrand(0, SimSize),
+        Y: rrand(0, SimSize),
+        DNA1: rrand(0, 2**32),
+        DNA2: rrand(0, 2**32),
+        ID: '0x'+(rrand(0, 2**32)>>>0).toString(16).toUpperCase(),
+    }
+}
+
+let grid = [];
+let population = [];
+
+function populateGrid() {
+    for (let i = 0; i < SimSize; i++) {
+        grid[i] = new Array(SimSize);
+    }
+
+    for (let i = 0; i < SimPopulation; i++) {
+        let a = NewRandomAgent()
+        while (grid[a.X][a.Y] != null) {
+            a = NewRandomAgent()
+        }
+        grid[a.X][a.Y] = a;
+        population.push(a);
+    }
+}
+
+function readAgent(a) {
+    return {
+        Color: {
+            R: ((a.DNA1&GeneMaskColor&0xF00) >> 8) * 16,
+            G: ((a.DNA1&GeneMaskColor&0x0F0) >> 4) * 16,
+            B:  (a.DNA1&GeneMaskColor&0x00F) * 16,
+        },
+    }
+}
+
+function agentAction(a, o) {
+    let dir = {1: 'left', 2: 'right', 3: 'up', 4: 'down'}[rrand(1, 4)];
+    return {
+        Action: ActionMove,
+        Direction: dir,
+    }
+}
+
+function observe(a) {
+    let depth = (a.DNA1&GeneMaskSight) >> 12;
+    let o = [];
+    const obs = (dir, dis, x) => x != null ? o.push({
+        direction: dir,
+        distance:  dis,
+        observation: readAgent(x),
+    }) : undefined;
+
+    if (a.X+1 < SimSize) for (let i = a.X+1; i <= Math.min(a.X+depth, SimSize-1); i++) obs('right', i, grid[i][a.Y]);
+    if (a.X-1 >= 0)      for (let i = a.X-1; i <= Math.max(a.X-depth, 0);         i--) obs('left',  i, grid[i][a.Y]);
+    if (a.Y+1 < SimSize) for (let i = a.Y+1; i <= Math.min(a.Y+depth, SimSize-1); i++) obs('down',  i, grid[a.X][i]);
+    if (a.Y-1 >= 0)      for (let i = a.Y-1; i <= Math.max(a.Y-depth, 0);         i--) obs('up',    i, grid[a.X][i]);
+
+    return o;
+}
+
+function agentMove(a, dir) {
+    let length = (a.DNA1&GeneMaskSpeed) >> 16;
+
+    grid[a.X][a.Y] = null;
+    switch (dir) {
+        case 'left':
+            a.X = Math.max(a.X - length, 0);
+            while (grid[a.X][a.Y] != null) {
+                a.X += 1;
+            }
+            break;
+        case 'right':
+            a.X = Math.min(a.X + length, SimSize-1);
+            while (grid[a.X][a.Y] != null) {
+                a.X -= 1;
+            }
+            break;
+        case 'up':
+            a.Y = Math.max(a.Y - length, 0);
+            while (grid[a.X][a.Y] != null) {
+                a.Y += 1;
+            }
+            break;
+        case 'down':
+            a.Y = Math.min(a.Y + length, SimSize-1);
+            while (grid[a.X][a.Y] != null) {
+                a.Y -= 1;
+            }
+            break;
+        default:
+            throw new Error("unknown direction: " + dir);
+    }
+    grid[a.X][a.Y] = a;
+}
+
+function performAction(agent, action) {
+    switch (action.Action) {
+        case ActionMove:
+            agentMove(agent, action.Direction);
+            break;
+        case ActionEat:
+            break;
+        case ActionMate:
+            break;
+        default:
+            throw new Error("unknown action: " + action);
+    }
+}
+
+
+/**
+ * EXECUTION LOOP
+ * Look Alive...
+ */
+populateGrid()
+
+let sim_log = {
+    size: SimSize,
+    population: SimPopulation,
+    totalEpochs: SimEpochs,
+    startedAt: Date.now(),
+    epoch: [
+        {
+            time: 0,
+            state: JSON.parse(JSON.stringify(population)),
+        },
+    ],
+};
+
+for (let e = 1; e <= SimEpochs; e++) {
+    const _start = Date.now();
+
+    for (let i = 0; i < population.length; i++) {
+        let agent = population[i];
+        let observation;
+        // try {
+        //     observation = observe(agent);
+        // } catch (err) {
+        //     console.error(err, agent, grid[agent.X]);
+        // }
+        let action = agentAction(agent, observation);
+
+        performAction(agent, action);
+    }
+
+    sim_log.epoch.push({
+        time: Date.now() - _start,
+        state: JSON.parse(JSON.stringify(population)),
+    });
+}
+
+console.log(JSON.stringify(sim_log));
